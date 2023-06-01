4d86fb0fa17097f946aac22cfa8f820e9fb83546
Alisan Cavus <alisan.cavus@linkus.eu>
2023-06-01 11:28:55 +0300

    separate cv upload

--- app/step3/UploadResume.tsx
+++ app/step3/UploadResume.tsx
@@ -1,26 +1,22 @@
 "use client";
-
 import { affindaClient } from "@/affinda.config";
 import { loadingContext } from "@/helpers";
-import { createMotivLetter } from "@/lib/createMotivLetter";
 import { ContextContentType } from "@/types/generalTypes";
 import { DocumentError } from "@affinda/affinda";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useRef, useState } from "react";
 import LoaderJobDescript from "../components/LoaderJobDescript";
 import WizardHeader from "../components/WizardHeader";
+import { useToastStore } from "@/store/store";
+import { IoMdCloseCircle } from "react-icons/io";
 
 const UploadResume = () => {
   const router = useRouter();
+  const { isToast, change } = useToastStore();
   const [error, setError] = useState<string | undefined | DocumentError>();
   const [isLoading, setIsLoading] = useState(false);
+  const [isResume, setIsResume] = useState({} as any);
   const fileInputRef = useRef<HTMLInputElement>(null);
-  const [motivLetter, setMotivLetter] = useState("");
-  const [letter, setLetter] = useState("");
-  const [stateResume, setStateResume] = useState({} as any);
-  const [jobDescription, setJobDescription] = useState(
-    localStorage.getItem("jobDescription") || ""
-  );
 
   const [context, setContext] = useState<ContextContentType>(
     loadingContext[Math.floor(Math.random() * loadingContext.length)]
@@ -28,7 +24,9 @@ const UploadResume = () => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setContext(loadingContext[Math.floor(Math.random() * loadingContext.length)]);
+      setContext(
+        loadingContext[Math.floor(Math.random() * loadingContext.length)]
+      );
     }, 5000);
 
     return () => clearInterval(interval);
@@ -48,48 +46,92 @@ const UploadResume = () => {
         setError(res.error);
       }
       const resume = res.data;
+
       // save to local storage as resume
       localStorage.setItem("resume", JSON.stringify(resume));
-      // create motivation letter
-      const motivationLetter = await createMotivLetter({
-        motivLetter,
-        setMotivLetter,
-        resume,
-        isLoading,
-        setIsLoading,
-        jobDescription,
-      });
-
-      if (fileInputRef.current) {
-        fileInputRef.current.value = "";
+      setIsResume(resume);
+      if (isResume){
+        router.push("/step4");
       }
     }
-    router.push("/step4");
   };
 
   if (isLoading) {
-    return <LoaderJobDescript context={context} status={"Uploading your CV and Parsing the information."} />;
+    return (
+      <LoaderJobDescript
+        context={context}
+        status={"Uploading your CV and Parsing the information."}
+      />
+    );
   }
 
   return (
+    <>
     <WizardHeader
       p={
-        "Now we need your CV for creating ultimate go getter motivation letter, to harden your application to this job."
+        "Now we need your CV for creating ultimate go-getter motivation letter, to harden your application to this job."
       }
       h2={"Upload your CV"}
     >
-       <div>
-        <input
-          type="file"
-          className="file-input-bordered file-input-secondary file-input w-full max-w-xs "
-          accept=".doc, .docx, .pdf"
-          ref={fileInputRef}
-          onChange={handleUpload}
-        />
+      <div className="flex h-full flex-col justify-center gap-4">
+        <div className="form-control mx-auto p-2">
+          <input
+            type="file"
+            className="file-input-bordered file-input-secondary file-input-lg w-full max-w-xs"
+            accept=".doc, .docx, .pdf"
+            ref={fileInputRef}
+            onChange={handleUpload}
+          />
+        </div>
+
+        <div className=" align-center flex h-full w-full justify-between gap-4">
+          <button
+            onClick={() => router.back()}
+            type="button"
+            className="btn text-white"
+          >
+            Previous Step
+          </button>
+          
+        </div>
       </div>
-      
     </WizardHeader>
+    {error && isToast && (
+      <div className="toast-center toast w-96">
+        <div className="alert alert-error">
+          <div>
+            <span>{error as string}</span>
+          </div>
+          <div className="flex-none">
+            <button
+              onClick={() => change(isToast)}
+              className="btn-error btn-ghost btn"
+            >
+              <IoMdCloseCircle className="h-6 w-6" />
+            </button>
+          </div>
+        </div>
+      </div>
+    )}
+    </>
   );
 };
 
 export default UploadResume;
+
+// const motivationLetter = await createMotivLetter({
+//   motivLetter,
+//   setMotivLetter,
+//   resume,
+//   isLoading,
+//   setIsLoading,
+//   jobDescription,
+// });
+// const [motivLetter, setMotivLetter] = useState("");
+// const [letter, setLetter] = useState("");
+// const [stateResume, setStateResume] = useState({} as any);
+// const [jobDescription, setJobDescription] = useState(
+//   localStorage.getItem("jobDescription") || ""
+// );
+
+// router.push("/step4");
